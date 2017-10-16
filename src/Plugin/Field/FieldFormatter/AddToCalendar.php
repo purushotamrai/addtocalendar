c443b9304bba07169865e3ba0bc4beeca090a598
guptahemant <guptahemant@3278073.no-reply.drupal.org>
2017-10-16 17:43:09 +0530

    Issue #2819970 by guptahemant: Add Separate Field Formatter for Addtocalendar

--- src/Plugin/Field/FieldFormatter/AddToCalendar.php
+++ src/Plugin/Field/FieldFormatter/AddToCalendar.php
@@ -2,13 +2,10 @@
 
 namespace Drupal\addtocalendar\Plugin\Field\FieldFormatter;
 
-use Drupal\Component\Utility\Html;
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\datetime\Plugin\Field\FieldFormatter\DateTimeDefaultFormatter;
-use Drupal\datetime_range\DateTimeRangeTrait;
 
 /**
  * Plugin implementation of the 'add_to_calendar' formatter.
@@ -17,20 +14,18 @@ use Drupal\datetime_range\DateTimeRangeTrait;
  *   id = "add_to_calendar",
  *   label = @Translation("Add to calendar"),
  *   field_types = {
- *     "datetime"
+ *     "add_to_calendar_field",
  *   }
  * )
  */
-class AddToCalendar extends DateTimeDefaultFormatter {
+class AddToCalendar extends FormatterBase {
 
   /**
    * {@inheritdoc}
    */
   public static function defaultSettings() {
-    $addtocalendar_settings = ['style' => 'glow_orange'];
     return [
-      'addtocalendar_show' => '1',
-      'addtocalendar_settings'=> $addtocalendar_settings,
+      // Implement default settings.
     ] + parent::defaultSettings();
   }
 
@@ -39,12 +34,6 @@ class AddToCalendar extends DateTimeDefaultFormatter {
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $form = parent::settingsForm($form, $form_state);
-    // dsm(get_class_methods($this));
-    // dsm($this->getThirdPartySettings());
-    $field_definition = $this->fieldDefinition;
-    $settings = $this->getSettings();
-    // dsm($settings);
-    $form += _addtocalendar_build_form($settings, $field_definition);
     return $form;
   }
 
@@ -55,46 +44,41 @@ class AddToCalendar extends DateTimeDefaultFormatter {
     $summary = [];
     $summary = parent::settingsSummary();
     // Implement settings summary.
-
     return $summary;
   }
 
-    /**
+  /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
-    $settings = $this->getSettings();
-    $multiple_value = $settings['addtocalendar_settings']['multiple_value'];
+    $field_definition = $this->fieldDefinition;
     foreach ($items as $delta => $item) {
-      if ($item->date) {
-        /** @var \Drupal\Core\Datetime\DrupalDateTime $date */
-        dsm($delta);
-
-        $show_calendar = false;
-        $date = $item->date;
-        $elements[$delta] = [
-          'date' => $this->buildDateWithIsoAttribute($date),
-        ];
-        if($multiple_value == 2){
-          $show_calendar = true;
-        }
-        else if($multiple_value == 1 && $delta == $settings['addtocalendar_settings']['delta']){
-          $show_calendar = true;
-        }
-        if($show_calendar)
-          $elements[$delta]['add_to_cal'] = ['#plain_text' => 'render add to calendar widget here'];
-
-        if (!empty($item->_attributes)) {
-          $elements[$delta]['#attributes'] += $item->_attributes;
-          // Unset field item attributes since they have been included in the
-          // formatter output and should not be rendered in the field template.
-          unset($item->_attributes);
-        }
-      }
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
     }
-    dsm($settings);
+
     return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    if ($item->value == 1) {
+      $return = $this->fieldDefinition->getSetting('on_label');
+    }
+    else {
+      $return = $this->fieldDefinition->getSetting('off_label');
+    }
+    return $return;
   }
 
 }
